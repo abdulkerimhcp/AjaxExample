13d729a0bd22bc989a7728a94a9dbcf5208b8a20
intimeFront <intimeinfo.api@gmail.com>
2021-06-12 09:01:40 +0300

    tek modaldan seçilen kullanıcı detay sayfası açtırma

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(function () {
     const apiKey = "6091431b34a224acec720aed";
 
     $.ajax({
@@ -8,40 +8,77 @@ $(document).ready(function () {
             'app-id': apiKey
         },
         success: function (response) {
-            let users = response.data;
-            for (let user of users) {
-                $('#data').append('<tr>\n' +
-                    '        <td>' + user.id + '</td>\n' +
-                    '        <td>' + user.title + '</td>\n' +
-                    '        <td>' + user.firstName + '</td>\n' +
-                    '        <td>' + user.lastName + '</td>\n' +
-                    '        <td>' + user.email + '</td>\n' +
-                    '        <td><img src="' + user.picture + '" alt=""></td>\n' +
-                    '        <td><button type="button" class="btn btn-info detay" data-toggle="modal" data-target="#exampleModalCenter">deneme</button></td>\n' +
-                    '    </tr>')
-            }
-        },
-        complete: function () {
-            let buttons = document.querySelectorAll('.detay')
-            buttons.forEach(function (button) {
-                'use strict';
-                button.onclick = function () {
-                    let userId = button.parentNode.parentNode.childNodes[1].textContent;
+            response.data.forEach(user => {
+                $('#data').append(
+                    `<tr>
+                <td>${user.id}</td>
+                <td>${user.title}</td>
+                <td>${user.firstName}</td>
+                <td>${user.lastName}</td>
+                <td>${user.email}</td>
+                <td>
+                    <img src="${user.picture}" alt="">
+                </td>
+                <td>
+                    <button type="button" class="btn btn-primary detail" data-toggle="modal" data-target="#${user.id}">
+                        Detay
+                    </button>
+                </td>
+            </tr>`
+                );
+            });
+            let detailButtons = $('.detail');
+            detailButtons.each(function () {
+                $(this).click(function () {
+                    let userId = $(this).attr("data-target");
                     $.ajax({
                         type: 'GET',
-                        url: 'https://dummyapi.io/data/api/user/' + userId + '',
+                        url: `https://dummyapi.io/data/api/user/${userId.replace('#', "")}`,
                         headers: {
                             'app-id': apiKey
                         },
                         success: function (response) {
-                            $('#userPhoto').empty().append('<img src="'+ response.picture +'" alt=""/>');
-                            $('.firsName').empty().append('<p>'+ response.firstName +'</p>');
-                            $('.lastName').empty().append('<p>'+ response.lastName +'</p>');
-                            $('.phone').empty().append('<p>'+ response.phone +'</p>');
+                            console.log(response)
+                            let modal = `<div class="modal fade" id="${userId}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                    <div id="detail" class="row">
+                                    <div id="userPhoto" class="col-5">
+                                    <img src="${response.picture}" alt="">
+                                </div>
+                                <div class="col-7">
+                                    <div class="firsName">${response.firstName}</div>
+                                    <div class="lastName">${response.lastName}</div>
+                                    <div class="phone">${response.phone}</div>
+                                </div>
+                                </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" onclick="close()" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`
+
+                            $('#modal').html(modal);
+                            $('.modal').modal("show");
+
                         },
                     });
-                };
+                });
             });
-        }
+
+        },
+
     });
+
 });
+
